935e5c9eb86f3657d386af9e60d5831e9d64b8b6
anirudharc <rcanirudha@gmail.com>
2018-11-29 16:01:13 -0500

    ucf dataloader

--- pose/__init__.py
+++ pose/__init__.py
@@ -6,6 +6,6 @@ from . import utils
 
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
-from progress.bar import Bar as Bar
+from .progress.bar import Bar as Bar
 
 __version__ = '0.1.0'
