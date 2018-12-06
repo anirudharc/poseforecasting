0982c370b1f78524aae94f80e64d6067f01d2a28
anirudharc <rcanirudha@gmail.com>
2018-12-06 15:36:13 -0500

    heatmap path arg

--- dataloader/ucf_dataloader.py
+++ dataloader/ucf_dataloader.py
@@ -18,34 +18,35 @@ RGB_DIR  = DATA_DIR + '/jpegs_256/'
 POSE_DIR = DATA_DIR + '/openpose_output/'
 UCF_LIST = BASE_DIR + '/dataloader/UCF_list/'
 VIDEO_DICT = BASE_DIR + '/dataloader/'
-
 CNV_POSE = "/media/bighdd1/arayasam/poseforecasting/preprocess/heatmaps/"
+
 # # Set randome seed
 # torch.random.manual_seed(args.seed)
 # np.random.seed(args.seed)
 # torch.cuda.manual_seed_all(args.seed) 
 
 class ucf_dataset(Dataset):  
-    def __init__(self, dic, rgb_dir, pose_dir, mode, transform=None):
+    def __init__(self, dic, rgb_dir, pose_dir, htmap_dir, mode, transform=None):
 
         self.keys = list(dic.keys())
         self.values= list(dic.values())
         self.rgb_dir = rgb_dir
         self.pose_dir = pose_dir
+        self.htmap_dir = htmap_dir
         self.mode =mode
         self.transform = transform
 
     def __len__(self):
         return len(self.keys)
 
-    def convert(self, pose_path, video_name):
+    def convert(self, pose_path, ht_path, video_name):
         
-        heatmap_path = os.path.join(CNV_POSE,video_name) + ".npz"
+        heatmap_path = os.path.join(ht_path,video_name) + ".npz"
         video_path = POSE_DIR + "v_" + video_name
 
         if os.path.isfile(heatmap_path):
             data = np.load(heatmap_path)
-            openpose_npy, occlusion, frame_mask = data['heatmap'], \
+            openpose_npy, occluion, frame_mask = data['heatmap'], \
                                     data['occlusion'], data['frame_mask']
 
         else:
@@ -76,18 +77,18 @@ class ucf_dataset(Dataset):
                     openpose_npy = np.append(openpose_npy, np.expand_dims(pose_np, axis=0), axis=0)
 
             #### Save as .npz file
-            save_path = CNV_POSE + "v_" + video_name + ".npz"
+            save_path = ht_path + "v_" + video_name + ".npz"
             np.savez(save_path, heatmap=openpose_npy, occlusion=occlusion, frameId=frame_mask)
 
         return openpose_npy, occlusion, frame_mask
 
-    def load_ucf_image(self, video_name):
+    def load_ucf_image(self, ht_path, video_name):
         rgb_path = self.rgb_dir + 'v_' + video_name + '/' + "frame000001.jpg"
         pose_path = self.pose_dir + 'v_' + video_name + '/'
 
         # image = Image.open(open(rgb_path, 'rb'))
         image = cv2.imread(rgb_path)
-        openpose_npy, occlusion, frame_mask = self.convert(pose_path, video_name)
+        openpose_npy, occlusion, frame_mask = self.convert(pose_path, self.htmap_dir, video_name)
 
         # # Transformations of images if needed
         # img = Image.fromarray(img)
@@ -113,11 +114,13 @@ class ucf_dataset(Dataset):
         data ={}
         if self.mode=='train':
             # Maintaining a dict for future extension of features
-            data['image'], data['heatmaps'], data['occlusion'], data['frameId'] = self.load_ucf_image(video_name)
+            data['image'], data['heatmaps'], data['occlusion'], data['frameId'] = \
+                                            self.load_ucf_image(self.htmap_dir, video_name)
             sample = (data, label)
 
         elif self.mode=='val':
-            data['image'], data['heatmaps'], data['occlusion'], data['frameId'] = self.load_ucf_image(video_name)
+            data['image'], data['heatmaps'], data['occlusion'], data['frameId'] = \
+                                            self.load_ucf_image(self.htmap_dir, video_name)
             sample = (data, label)
         else:
             raise ValueError('There are only train and val mode')
@@ -125,12 +128,14 @@ class ucf_dataset(Dataset):
         return sample
 
 class ucf_dataloader():
-    def __init__(self, BATCH_SIZE, num_workers, rgb_path, pose_path, ucf_list, ucf_split):
+    def __init__(self, BATCH_SIZE, num_workers, rgb_path, pose_path, htmap_path, ucf_list, ucf_split):
 
         self.BATCH_SIZE=BATCH_SIZE
         self.num_workers=num_workers
         self.rgb_path=rgb_path
         self.pose_path=pose_path
+        self.htmap_path=htmap_path
+
         self.frame_count={}
         self.train_video, self.val_video, self.test_video = self.load_video_dict(ucf_list, ucf_split)
         
@@ -185,7 +190,8 @@ class ucf_dataloader():
         return train_loader, val_loader, test_loader
 
     def train(self):
-        training_set = ucf_dataset(dic=self.train_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, mode='train', transform = transforms.Compose([
+        training_set = ucf_dataset(dic=self.train_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, htmap_dir=self.htmap_path, mode='train', 
+            transform = transforms.Compose([
                 transforms.RandomCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
@@ -201,7 +207,8 @@ class ucf_dataloader():
         return train_loader
 
     def validate(self):
-        validation_set = ucf_dataset(dic=self.val_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, mode='val', transform = transforms.Compose([
+        validation_set = ucf_dataset(dic=self.val_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, htmap_dir=self.htmap_path, mode='val', 
+            transform = transforms.Compose([
                 transforms.Scale([224,224]),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
@@ -217,7 +224,8 @@ class ucf_dataloader():
         return val_loader
 
     def test(self):
-        test_set = ucf_dataset(dic=self.test_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, mode='train', transform = transforms.Compose([
+        test_set = ucf_dataset(dic=self.test_video, rgb_dir=self.rgb_path, pose_dir=self.pose_path, htmap_dir=self.htmap_path, mode='train', 
+            transform = transforms.Compose([
                 transforms.RandomCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
@@ -237,6 +245,7 @@ if __name__ == '__main__':
     dataloader = ucf_dataloader(BATCH_SIZE=1, num_workers=1, 
                                 rgb_path=RGB_DIR,
                                 pose_path=POSE_DIR,
+                                htmap_path=CNV_POSE,
                                 ucf_list=UCF_LIST,
                                 ucf_split='01')
     train_loader, val_loader, test_loader = dataloader.run()
